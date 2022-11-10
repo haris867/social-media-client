774e70ab24e4cee238c2a6f798ff8b98cb27a47b
Haris <haris.usman@hotmail.com>
2022-11-10 23:03:07 +0100

    Created cypress test for login form

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,42 @@
-const validEmail = 'Cypresstest@noroff.no';
+const validEmail = 'cypresstest@noroff.no';
 const validPassword = 'Cypress123';
 
 const invalidEmail = 'myfake@email.com';
 const invalidPassword = 'pasword';
 
 describe('Authentication', () => {
-  it('can login', () => {
+  beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/');
+    cy.clearLocalStorage();
+  });
+
+  it('Allows user with valid credentials to log in', () => {
+    cy.wait(500);
+    cy.get('#registerForm .btn-close').click();
+    cy.wait(400);
+    cy.get('button[data-auth="login"]:visible').click();
+    cy.wait(400);
+    cy.get('input[type="email"]:visible').type(validEmail);
+    cy.wait(1000);
+    cy.get('input[type="password"]:visible').type(validPassword);
+    cy.wait(1000);
+    cy.get('.btn-success:visible').click();
+    cy.wait(400);
+    cy.then(() => expect(localStorage.getItem('token')).to.not.be.null);
+  });
+
+  it('Restricts user with invalid credentials from logging in', () => {
+    cy.wait(500);
+    cy.get('#registerForm .btn-close').click();
+    cy.wait(400);
+    cy.get('button[data-auth="login"]:visible').click();
+    cy.wait(400);
+    cy.get('input[type="email"]:visible').type(invalidEmail);
+    cy.wait(1000);
+    cy.get('input[type="password"]:visible').type(invalidPassword);
+    cy.wait(1000);
+    cy.get('.btn-success:visible').click();
+    cy.wait(400);
+    cy.then(() => expect(localStorage.getItem('token')).to.be.null);
   });
 });
